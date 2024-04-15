f0ff3e1ef2ca5d6797e7324e7c6c8e17200ff20a
tlun0911 <164435147+tlun0911@users.noreply.github.com>
2024-04-15 16:26:51 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,37 +7,69 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  console.log(users);
+  let newUser = req.body.username;
+  let newPassword = req.body.password;
+  if(!newUser.isValid){
+    return res.send("Please enter a username and password");
+  }
+  else if (newUser in users){
+    return res.send("User is already registered");
+  }
+  else{
+    users.push({"userName": newUser, "password": newPassword});
+    res.send("User has been added!");
+  }
+  console.log(users);
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let newISBN = req.params.isbn;
+    return res.send(JSON.stringify(books[newISBN]), null, 4);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const newAuthor = req.params.author;
+  const keys = Object.keys(books);
+  keys.forEach((key) => {
+    let tempAuthor = books[key];
+        if (tempAuthor.author === newAuthor) {
+            return res.send(JSON.stringify(books[key], null, 4))
+        }
+
+    });    
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const newTitle= req.params.title;
+  const keys = Object.keys(books);
+  keys.forEach((key) => {
+    let tempTitle = books[key];
+    if(tempTitle.title === newTitle)   {
+        return res.send(JSON.stringify(books[key], null, 4));
+    } 
+  });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let newISBN = req.params.isbn;
+  let newReview = books[newISBN];
+  return res.send(JSON.stringify(newReview.review), null, 4);
 });
 
 module.exports.general = public_users;
